60522b2b7e56189f4fa3774afd2682ad93e42054
UtkanKarahan <102539382+UtkanKarahan@users.noreply.github.com>
2022-05-24 17:08:30 +0300

    --

--- plunder/lib/main.dart
+++ plunder/lib/main.dart
@@ -1,115 +1,160 @@
+import 'package:firebase_crashlytics/firebase_crashlytics.dart';
+import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
+import 'package:go_router/go_router.dart';
+import 'package:logging/logging.dart';
+import 'package:plunder/src/app_lifecycle/app_lifecycle.dart';
+import 'package:plunder/src/crashlytics/crashlytics.dart';
+import 'package:provider/provider.dart';
+import 'src/games_services/games_services.dart';
+import 'src/main_menu/main_menu_screen.dart';
+import 'src/settings/persistence/local_storage_settings_persistence.dart';
+import 'src/settings/persistence/settings_persistence.dart';
+import 'src/settings/settings.dart';
+import 'src/settings/settings_screen.dart';
+import 'src/style/palette.dart';
+import 'src/style/snack_bar.dart';
 
-void main() {
-  runApp(const MyApp());
+
+Future<void> main() async {
+  // Uncomment the following lines to enable Firebase Crashlytics.
+  // See lib/src/crashlytics/README.md for details.
+
+  FirebaseCrashlytics? crashlytics;
+  // if (!kIsWeb && (Platform.isIOS || Platform.isAndroid)) {
+  //   try {
+  //     WidgetsFlutterBinding.ensureInitialized();
+  //     await Firebase.initializeApp(
+  //       options: DefaultFirebaseOptions.currentPlatform,
+  //     );
+  //     crashlytics = FirebaseCrashlytics.instance;
+  //   } catch (e) {
+  //     debugPrint("Firebase couldn't be initialized: $e");
+  //   }
+  // }
+
+  await guardWithCrashlytics(
+    guardedMain,
+    crashlytics: crashlytics,
+  );
 }
+
+/// Without logging and crash reporting, this would be `void main()`.
+void guardedMain() {
+  if (kReleaseMode) {
+    // Don't log anything below warnings in production.
+    Logger.root.level = Level.WARNING;
+  }
+  Logger.root.onRecord.listen((record) {
+    debugPrint('${record.level.name}: ${record.time}: '
+        '${record.loggerName}: '
+        '${record.message}');
+  });
+
+  WidgetsFlutterBinding.ensureInitialized();
+
+  _log.info('Going full screen');
+  SystemChrome.setEnabledSystemUIMode(
+    SystemUiMode.edgeToEdge,
+  );
+
+
+
+
+  GamesServicesController? gamesServicesController;
+  // if (!kIsWeb && (Platform.isIOS || Platform.isAndroid)) {
+  //   gamesServicesController = GamesServicesController()
+  //     // Attempt to log the player in.
+  //     ..initialize();
+  // }
+
+
+
+  runApp(
+    MyApp(
+      settingsPersistence: LocalStorageSettingsPersistence(),
+
+      gamesServicesController: gamesServicesController,
+    ),
+  );
+}
+
+Logger _log = Logger('main.dart');
 
 class MyApp extends StatelessWidget {
-  const MyApp({Key? key}) : super(key: key);
+  static final _router = GoRouter(
+    routes: [
+      GoRoute(
+          path: '/',
+          builder: (context, state) =>
+          const MainMenuScreen(key: Key('main menu')),
+          routes: [
 
-  // This widget is the root of your application.
-  @override
-  Widget build(BuildContext context) {
-    return MaterialApp(
-      title: 'Flutter Demo',
-      theme: ThemeData(
-        // This is the theme of your application.
-        //
-        // Try running your application with "flutter run". You'll see the
-        // application has a blue toolbar. Then, without quitting the app, try
-        // changing the primarySwatch below to Colors.green and then invoke
-        // "hot reload" (press "r" in the console where you ran "flutter run",
-        // or simply save your changes to "hot reload" in a Flutter IDE).
-        // Notice that the counter didn't reset back to zero; the application
-        // is not restarted.
-        primarySwatch: Colors.blue,
-      ),
-      home: const MyHomePage(title: 'Flutter Demo Home Page'),
-    );
-  }
-}
-
-class MyHomePage extends StatefulWidget {
-  const MyHomePage({Key? key, required this.title}) : super(key: key);
-
-  // This widget is the home page of your application. It is stateful, meaning
-  // that it has a State object (defined below) that contains fields that affect
-  // how it looks.
-
-  // This class is the configuration for the state. It holds the values (in this
-  // case the title) provided by the parent (in this case the App widget) and
-  // used by the build method of the State. Fields in a Widget subclass are
-  // always marked "final".
-
-  final String title;
-
-  @override
-  State<MyHomePage> createState() => _MyHomePageState();
-}
-
-class _MyHomePageState extends State<MyHomePage> {
-  int _counter = 0;
-
-  void _incrementCounter() {
-    setState(() {
-      // This call to setState tells the Flutter framework that something has
-      // changed in this State, which causes it to rerun the build method below
-      // so that the display can reflect the updated values. If we changed
-      // _counter without calling setState(), then the build method would not be
-      // called again, and so nothing would appear to happen.
-      _counter++;
-    });
-  }
-
-  @override
-  Widget build(BuildContext context) {
-    // This method is rerun every time setState is called, for instance as done
-    // by the _incrementCounter method above.
-    //
-    // The Flutter framework has been optimized to make rerunning build methods
-    // fast, so that you can just rebuild anything that needs updating rather
-    // than having to individually change instances of widgets.
-    return Scaffold(
-      appBar: AppBar(
-        // Here we take the value from the MyHomePage object that was created by
-        // the App.build method, and use it to set our appbar title.
-        title: Text(widget.title),
-      ),
-      body: Center(
-        // Center is a layout widget. It takes a single child and positions it
-        // in the middle of the parent.
-        child: Column(
-          // Column is also a layout widget. It takes a list of children and
-          // arranges them vertically. By default, it sizes itself to fit its
-          // children horizontally, and tries to be as tall as its parent.
-          //
-          // Invoke "debug painting" (press "p" in the console, choose the
-          // "Toggle Debug Paint" action from the Flutter Inspector in Android
-          // Studio, or the "Toggle Debug Paint" command in Visual Studio Code)
-          // to see the wireframe for each widget.
-          //
-          // Column has various properties to control how it sizes itself and
-          // how it positions its children. Here we use mainAxisAlignment to
-          // center the children vertically; the main axis here is the vertical
-          // axis because Columns are vertical (the cross axis would be
-          // horizontal).
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            const Text(
-              'You have pushed the button this many times:',
+            GoRoute(
+              path: 'settings',
+              builder: (context, state) =>
+              const SettingsScreen(key: Key('settings')),
             ),
-            Text(
-              '$_counter',
-              style: Theme.of(context).textTheme.headline4,
-            ),
-          ],
+          ]),
+    ],
+  );
+
+
+  final SettingsPersistence settingsPersistence;
+
+  final GamesServicesController? gamesServicesController;
+
+
+
+  const MyApp({
+  required this.settingsPersistence,
+  required this.gamesServicesController,
+  super.key,
+});
+
+@override
+Widget build(BuildContext context) {
+  return AppLifecycleObserver(
+    child: MultiProvider(
+      providers: [
+
+        Provider<GamesServicesController?>.value(
+            value: gamesServicesController),
+
+        Provider<SettingsController>(
+          lazy: false,
+          create: (context) => SettingsController(
+            persistence: settingsPersistence,
+          )..loadStateFromPersistence(),
         ),
-      ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: _incrementCounter,
-        tooltip: 'Increment',
-        child: const Icon(Icons.add),
-      ), // This trailing comma makes auto-formatting nicer for build methods.
-    );
-  }
+
+        Provider(
+          create: (context) => Palette(),
+        ),
+      ],
+      child: Builder(builder: (context) {
+        final palette = context.watch<Palette>();
+
+        return MaterialApp.router(
+          title: 'Plunder NFT TCG',
+          theme: ThemeData.from(
+            colorScheme: ColorScheme.fromSeed(
+              seedColor: palette.darkPen,
+              background: palette.backgroundMain,
+            ),
+            textTheme: TextTheme(
+              bodyText2: TextStyle(
+                color: palette.ink,
+              ),
+            ),
+          ),
+          routeInformationParser: _router.routeInformationParser,
+          routerDelegate: _router.routerDelegate,
+          scaffoldMessengerKey: scaffoldMessengerKey,
+        );
+      }),
+    ),
+  );
+}
 }
